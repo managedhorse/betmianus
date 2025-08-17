10c408294718237d2a1361739a951993c9662f4e
managedhorse <david.webpersonal@gmail.com>
2025-08-17 19:40:49 +0800

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppKitProvider } from './AppKitProvider';
 import BetMianus from './pages/BetMianus.jsx';
 import Logout from './pages/Logout.jsx';
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx'; // <-- add this
 import '@reown/appkit-wallet-button';
-
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<BetMianus />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} /> {/* <-- new route */}
         </Routes>
       </BrowserRouter>
     </AppKitProvider>
